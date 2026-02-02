9fb9a147f10a43143c576ebd68ecc98e58fa6aa0
AMOGM2004 <amog.m2004@gmail.com>
2026-02-02 13:02:18 +0530

    Fix Vercel configuration

--- server/server.js
+++ server/server.js
@@ -1,69 +1,19 @@
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const path = require('path');
-const DrawingState = require('./drawing-state');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static('client'));
 
-// Basic route
+// Simple route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(__dirname + '/../client/index.html');
 });
 
-// Store connected users
-const users = new Map();
-const drawingState = new DrawingState();
-
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-    
-    const userColor = `hsl(${Math.random() * 360}, 70%, 60%)`;
-    users.set(socket.id, { id: socket.id, color: userColor });
-    
-    socket.emit('init', { 
-        userId: socket.id, 
-        color: userColor,
-        users: Array.from(users.values())
-    });
-    
-    socket.broadcast.emit('user-joined', users.get(socket.id));
-    
-    socket.on('disconnect', () => {
-        users.delete(socket.id);
-        socket.broadcast.emit('user-left', socket.id);
-    });
-    
-    socket.on('draw', (data) => {
-        socket.broadcast.emit('draw', { ...data, userId: socket.id });
-    });
-    
-    socket.on('cursor-move', (position) => {
-        socket.broadcast.emit('cursor-update', {
-            userId: socket.id,
-            cursor: position
-        });
-    });
-});
-
-// For Vercel
+// WebSocket not needed for basic deployment test
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-// Export for Vercel serverless
-if (process.env.VERCEL) {
-    module.exports = (req, res) => {
-        // Handle HTTP requests
-        app(req, res);
-    };
-} else {
-    // For local development
-    server.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-    module.exports = server;
-}
+// Export for Vercel
+module.exports = app;
